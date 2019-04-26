3020f5f32b108ee4bf94ae7e0730682efc7904ec
peter max <petermax2004@gmail.com>
2019-04-26 17:41:59 +0300

    Add to the js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
-var answer =function() {
+function() {
 var dimension1=parseInt(document.getElementById("Dimension 1").value);
 
 var dimension2=parseInt(document.getElementById("Dimension 2").value);
 
 var dimension3=parseInt(document.getElementById("Dimension 3").value);
 
-var answer=document.getElementById("answer");
+var answer=document.getElementsById("answer");
 
 if(Dimension 1===Dimension 2 && Dimension 1===Dimension 3) {
-return(answer("Equilateral Triangle!"));
+alert(answer("Equilateral Triangle!"));
 
 }else if (Dimension 2===Dimension 1||Dimension 1===Dimension 3||Dimension 2===Dimension 3) {
-return(answer("Isosceles Triangle!"));
+alert(answer("Isosceles Triangle!"));
 
 }else {
-return(answer("Scalene Triangle!"));
+alert(answer("Scalene Triangle!"));
 }
 }
