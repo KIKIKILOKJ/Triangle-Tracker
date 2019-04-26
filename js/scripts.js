f8d8a110051e8da3bbd54f76c9ac351f14873fbb
peter max <petermax2004@gmail.com>
2019-04-26 16:54:10 +0300

    Add codes to the webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
-function answer() {
+var answer =function() {
 var dimension1=parseInt(document.getElementById("Dimension 1").value);
+
 var dimension2=parseInt(document.getElementById("Dimension 2").value);
+
 var dimension3=parseInt(document.getElementById("Dimension 3").value);
 
-var answer=document.getElementById("answer")
-if(Dimension1===Dimension2 && Dimension1===Dimension3) {
-alert ("Equilateral Triangle!");
-}else if (Dimension2===Dimension1||Dimension1==Dimension3||Dimension2===Dimension3) {
-alert ("Isosceles Triangle!");
+var answer=document.getElementById("answer");
+
+if(Dimension 1===Dimension 2 && Dimension 1===Dimension 3) {
+return(answer("Equilateral Triangle!"));
+
+}else if (Dimension 2===Dimension 1||Dimension 1===Dimension 3||Dimension 2===Dimension 3) {
+return(answer("Isosceles Triangle!"));
+
 }else {
-alert ("Scalene Triangle!")
+return(answer("Scalene Triangle!"));
 }
 }
