5a51a4bf56bccaced96529a0d454cd5985ab44da
peter max <petermax2004@gmail.com>
2019-04-28 12:34:38 +0300

    Add codes to the file

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
-var result = function() {
-var dimension1=parseInt(document.getElementById("Dimension 1").value);
-
-var dimension2=parseInt(document.getElementById("Dimension 2").value);
-
-var dimension3=parseInt(document.getElementById("Dimension 3").value);
-
-
-if(Dimension1===Dimension2 && Dimension2===Dimension3){
-return("Equilateral Triangle");
-
-}else if (Dimension2===Dimension1||Dimension1===Dimension3||Dimension2===Dimension3){
-return("Isosceles Triangle");
-
-}else {
-return("Scalene Triangle");
+function result(){
+var side1=parseInt(document.getElementById('side1').value);
+var side2=parseInt(document.getElementById('side2').value);
+var side3=parseInt(document.getElementById('side3').value);
+var answer=document.getElementById('answer');
+if(side1+side2>side3 && side2+side3>side1 && side1+side3>side2) {
+  if (side1==side2&&side1==side3) {
+    answer.textContent='Equilateral: All sides are equal!';
+  }
+  else if(side1===side2||side2===side3||side1===side3){
+    answer.textContent='Isosceles: Two sides are equal!'
+  }
+  else{
+    answer.textContent="Scalene: No sides are equal!";
+  }
+}
+ else {
+  answer.textContent="Not a triangle!";
 }
 }
