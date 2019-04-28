463532030904f853d0583564793f03e9a60bb28b
peter max <petermax2004@gmail.com>
2019-04-28 10:48:38 +0300

    Add to the webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,18 @@
-function() {
+var result = function() {
 var dimension1=parseInt(document.getElementById("Dimension 1").value);
 
 var dimension2=parseInt(document.getElementById("Dimension 2").value);
 
 var dimension3=parseInt(document.getElementById("Dimension 3").value);
 
-var answer=document.getElementsById("answer");
 
-if(Dimension 1===Dimension 2 && Dimension 1===Dimension 3) {
-alert(answer("Equilateral Triangle!"));
+if(Dimension1===Dimension2 && Dimension2===Dimension3){
+return("Equilateral Triangle");
 
-}else if (Dimension 2===Dimension 1||Dimension 1===Dimension 3||Dimension 2===Dimension 3) {
-alert(answer("Isosceles Triangle!"));
+}else if (Dimension2===Dimension1||Dimension1===Dimension3||Dimension2===Dimension3){
+return("Isosceles Triangle");
 
 }else {
-alert(answer("Scalene Triangle!"));
+return("Scalene Triangle");
 }
 }
